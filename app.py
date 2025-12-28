b574ee7bb581b9bc5018c90aee767f0e4fcd9dcd
Sahil Kumar <133761749+Sahilx987@users.noreply.github.com>
2025-12-28 23:43:48 +0530

    Change greeting message in Flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "CI/CD working via Jenkins"
+    return "Hello Sahil"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
